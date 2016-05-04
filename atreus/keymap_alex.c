936fdf18a31bc7c7beedd4c31998e1657f684d99
Alex Flores <eflores@mdsol.com>
2016-05-04 12:08:55 -0400

    swap control and command

--- atreus/keymap_alex.c
+++ atreus/keymap_alex.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM actionmaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TAP(5,Q) , W        , E    , R    , T                  , Y    , U    , I   , O    , P    , \
       A        , TAP(4,S) , D    , F    , G                  , H    , J   , K    , L    , SCLN , \
       TAP(3,Z) , X        , C    , V    , B                  , N    , M   , COMM , DOT  , SLSH , \
-      ESC      , TAB      , LALT , LSFT , BSPC , LCTL , LGUI , SPC , LM1  , MINS , QUOT , ENT
+      ESC      , TAB      , LALT , LSFT , BSPC , LGUI , LCTL , SPC , LM1  , MINS , QUOT , ENT
   ),
 
   KEYMAP( /* 1: punctuation and numbers */
@@ -40,21 +40,21 @@ const uint16_t PROGMEM actionmaps[][MATRIX_ROWS][MATRIX_COLS] = {
       INS , HOME , UP   , END   , PGUP               , UP   , F7     , F8  , F9  , F10 , \
       DEL , LEFT , DOWN , RIGHT , PGDN               , DOWN , F4     , F5  , F6  , F11 , \
       NO  , NO   , NO   , NO    , BOOT               , NO   , F1     , F2  , F3  , F12 , \
-      NO  , LSFT , LALT , LSFT  , BSPC , LCTL , LGUI , SPC  , OFF(2) , NO  , NO  , ENT
+      NO  , LSFT , LALT , LSFT  , BSPC , LGUI , LCTL , SPC  , OFF(2) , NO  , NO  , ENT
   ),
 
   KEYMAP( /* 3: mouse */
       NO   , NO   , NO   , NO   , NO                 , WH_D , BTN1   , MS_U , BTN2 , NO , \
       ACL0 , ACL1 , ACL2 , NO   , NO                 , WH_U , MS_L   , MS_D , MS_R , NO , \
       TRNS , NO   , NO   , NO   , NO                 , NO   , NO     , NO   , NO   , NO , \
-      ESC  , LSFT , LALT , LSFT , BSPC , LCTL , LGUI , SPC  , OFF(3) , NO   , NO   , ENT
+      ESC  , LSFT , LALT , LSFT , BSPC , LGUI , LCTL , SPC  , OFF(3) , NO   , NO   , ENT
   ),
 
   KEYMAP( /* 4: quick arrows, tiling,  and media */
       NO  , NO   , NO   , NO   , NO                 , MPRV , MPLY   , MNXT , VOLD  , VOLU , \
       NO  , TRNS , NO   , NO   , NO                 , LEFT , DOWN   , UP   , RIGHT , MUTE , \
       NO  , NO   , NO   , NO   , NO                 , TPMN , TLFT   , TFLS , TLRT  , TNMN , \
-      ESC , LSFT , LALT , LSFT , DEL  , LCTL , LGUI , SPC  , OFF(4) , NO   , NO    , ENT
+      ESC , LSFT , LALT , LSFT , DEL  , LGUI , LCTL , SPC  , OFF(4) , NO   , NO    , ENT
   ),
 
   KEYMAP( /* 5: quick number selections */
