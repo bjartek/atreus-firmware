2f65fc8d38140d35aed2d1913e91be8cce5034e1
Bjarte Stien Karlsen <bjarte@bjartek.org>
2016-08-30 22:54:18 +0200

    Changed layout completely. No more holding homerow keys, two main layers on space and backspace.
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- atreus/keymap_bjartek.c
+++ atreus/keymap_bjartek.c
@@ -17,40 +17,29 @@
 
 const uint16_t PROGMEM actionmaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP( /* 0: mostly letters */
-    Q,               W,        E,         R,         T,    /*|,, |*/             Y,   U,   I,    O,    P,    \
-    TAP(5,A),        TAP(2,S), TAP(3,D),  TAP(4,F),  G,    /*|,, |*/             H,   J,   K,    L,    SCLN, \
-    Z,               X,        C,         V,         B,    /*|,, |*/             N,   M,   COMM, DOT,  SLSH, \
-    TAPK(RALT, ESC), TAB,      OSM(LGUI), OSM(LSFT), BSPC, OSM(LCTL), OSM(LALT), SPC, TAP(1,TAB), MINS, QUOT, TAPK(RALT,ENT)
+    Q,               W,     E,         R,         T,   					 /*|,, |*/          Y,          U,   I,    O,    P,    \
+    A,               S,     D,         F,         G,   					 /*|,, |*/          H,          J,   K,    L,    SCLN, \
+    Z,               X,     C,         V,         B,   					 /*|,, |*/          N,          M,   COMM, DOT,  SLSH, \
+    TAPK(RALT, ESC), ON(2), OSM(LGUI), OSM(LSFT), TAP(1,BSPC), OSM(LCTL), OSM(LALT), TAP(2,SPC), TAP(3,TAB), MINS, QUOT, TAPK(RALT,ENT)
   ),
   KEYMAP( /* layer one is mostly for programming and shell. lots of idea shortcute on left, not sure how much i will use them.*/
-    ACK(7),          CTRL(W),    ACSK(T),    SH(F10),     ACSK(L),                  SH(6), SH(LBRC), SH(RBRC), SH(7),    SH(1), \
-    CSK(A),          CSK(SPACE), CSK(ENT),   ALT(ENT),    ALT(INS),                 0,     SH(9)   , SH(0),    SH(BSLS), SH(4), \
-    ACSK(N),         CSK(F),     CSK(R),     ALT(V),      BOOT,                     GRAVE, LBRC    , RBRC,     SH(3),    SH(GRAVE), \
-    TAPK(RALT, ESC), TAB,        OSM(LGUI),  OSM(LSFT),   CS, OSM(LCTL), OSM(LALT), AS,    TRNS,      MINS,     SH(2),   TAPK(RALT,ENT)
+    ACK(7),          CTRL(W),    ACSK(T),    SH(F10),     ACSK(L),                    SH(6), SH(LBRC), SH(RBRC), SH(7),    SH(1), \
+    CSK(A),          CSK(SPACE), CSK(ENT),   ALT(ENT),    ALT(INS),                   0,     SH(9)   , SH(0),    SH(BSLS), SH(4), \
+    ACSK(N),         CSK(F),     CSK(R),     ALT(V),      BOOT,                       GRAVE, LBRC    , RBRC,     SH(3),    SH(GRAVE), \
+    TAPK(RALT, ESC), TAB,        OSM(LGUI),  OSM(LSFT),   TRNS, OSM(LCTL), OSM(LALT), SPC,   OFF(1) ,      MINS,    SH(2),    TAPK(RALT,ENT)
   ),
-  KEYMAP( /* FN pad while holding f */
-    NO,     NO,    NO,        NO,        NO,                         F1,  F2,  F3,   F4,   F5,  \
-    NO,     TRNS,  NO,        NO,        NO,                         F6,  F7,  F8,   F9,   F10, \
-    NO,     NO,    NO,        NO,        BOOT,                       F11, F12, F13,  F14,  F15, \
-    OFF(2), TAB,   OSM(LGUI), OSM(LSFT), CS,   OSM(LCTL), OSM(LALT), AS,  AC,  MINS, QUOT, TAPK(RALT,ENT)
+  KEYMAP( /* hold space brings up move pad and numpad */
+    INS,      HOME,   UP  ,      END  ,     PGUP,                         SH(5),7     , 8  , 9, SH(8),     \
+    DEL,      LEFT,   DOWN,      RIGHT,     PGDN,                         MINS, 4     , 5  , 6, SH(EQUAL), \
+    GUI(1),   GUI(2), GUI(3),    GUI(4),    GUI(5),                       NO,   1     , 2  , 3, BSLS,      \
+    ACK(DEL), GUI(D), OSM(LGUI), OSM(LSFT), CSK(Q),OSM(LCTL), OSM(LALT),  TRNS, OFF(2), DOT, 0, EQUAL
   ),
-  KEYMAP( /* numpad while holding d */
-    NO,     NO,  NO,        NO,        NO,                       SH(5),     7  , 8  , 9, SH(8),     \
-    NO,     NO,  TRNS,      OSM(LGUI), NO,                       MINS     , 4  , 5  , 6, SH(EQUAL), \
-    NO,     NO,  NO,        NO,        BOOT,                     NO       , 1  , 2  , 3, BSLS,      \
-    OFF(3), TAB, OSM(LGUI), OSM(LSFT), CS, OSM(LCTL), OSM(LALT), AS,        AC,  DOT, 0, EQUAL
+  KEYMAP( /* hold tab to have fpad and mouse */
+    F1,     F2,    F3,        F4,         F5,                             WH_D , BTN1   , MS_U , BTN2 , BTN3 , \
+    F6,     F7,    F8,        F9,         F10,                            WH_U , MS_L   , MS_D , MS_R , BTN4 , \
+    F11,    F12,   F13,       F14,        F15,                            NO   , ACL0   , ACL1 , ACL2 , BTN5 , \
+    OFF(3), TAB,   OSM(LGUI), OSM(LSFT), CAPSLOCK, OSM(LCTL), OSM(LALT),  SPC,    TRNS,      MINS,  QUOT,  TAPK(RALT,ENT)
   ),
-  KEYMAP( /* movement pad while holding down s */
-    NO,     NO,   NO,        NO,        NO,                       PGUP,   HOME,   UP  ,   END  ,  INS,   \
-    GSK(Q), NO,   NO,        TRNS,      NO,                       PGDN,   LEFT,   DOWN,   RIGHT,  DEL, \
-    NO,     NO,   NO,        NO,        BOOT,                     GUI(1), GUI(2), GUI(3), GUI(4), GUI(5),    \
-    OFF(4), TAB,  OSM(LGUI), OSM(LSFT), CS, OSM(LCTL), OSM(LALT), AS,     AC,     MINS,   QUOT,   ACK(DEL)
-  ),
-  KEYMAP( /* mouse pad while holding down a */
-    NO,     NO   , NO,        NO   ,     NO,                       WH_D , BTN1   , MS_U , BTN2 , BTN3 , \
-    TRNS,   ACL0 , ACL1,      ACL2 ,     NO,                       WH_U , MS_L   , MS_D , MS_R , BTN4 , \
-    NO,     NO   , NO,        NO   ,     NO,                       NO   , WH_L   , NO   , WH_R , BTN5 , \
-    OFF(5), TAB,   OSM(LGUI), OSM(LSFT), CS, OSM(LCTL), OSM(LALT), AS,    AC,      MINS,  QUOT,  TAPK(RALT,ENT)
-  ),
+
 };
 
