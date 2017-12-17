c264ad706d40298e03f183634a4ecc37e771a17a
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2017-12-17 20:29:51 +0100

    fix spelling errors

--- atreus/keymap_common.h
+++ atreus/keymap_common.h
@@ -88,6 +88,7 @@ extern const uint16_t actionmaps[][MATRIX_ROWS][MATRIX_COLS];
 #define AC_ACS ACTION_MODS(MOD_LALT | MOD_LCTL | MOD_LSFT)
 
 #define  AC_ACK(key) ACTION_MODS_KEY(MOD_LALT | MOD_LCTL,KC_##key)
+#define  AC_AGK(key) ACTION_MODS_KEY(MOD_LALT | MOD_LGUI,KC_##key)
 #define  AC_CSK(key) ACTION_MODS_KEY(MOD_LSFT | MOD_LCTL,KC_##key)
 #define  AC_GSK(key) ACTION_MODS_KEY(MOD_LSFT | MOD_LGUI,KC_##key)
 #define  AC_ASK(key) ACTION_MODS_KEY(MOD_LALT | MOD_LSFT,KC_##key)
